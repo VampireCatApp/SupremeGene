4e512fc87a9cdc35f0687871c2e39773cadc4d73
Marcin Mazur <marcin3mazur@gmail.com>
2017-09-05 18:53:49 +0200

    set main class as Game extention

--- core/src/com/mygdx/game/SupremeGene.java
+++ core/src/com/mygdx/game/SupremeGene.java
@@ -1,33 +1,31 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SupremeGene extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import com.badlogic.gdx.Game;
+
+public class SupremeGene extends Game {
+	private boolean paused;
+	public static final String GAME_NAME = "SupremeGene";
+
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		init();
+		//this.setScreen(new SplashScreen(this));
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	private void init() {
+		//TODO servers initiation
+	}
+
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
